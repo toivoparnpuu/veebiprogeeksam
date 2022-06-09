461d66cefe6a3db78b4f2f47659d7f492a4f9cb4
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-06-09 14:51:14 +0300

    featured images

--- functions.php
+++ functions.php
@@ -4,15 +4,5 @@
     wp_enqueue_style( 'my_style', get_template_directory_uri() . '/css/style.css' );
   }
   add_action('wp_enqueue_scripts', 'my_style_enqueue_styles');
-
-
-  function register_my_links_menu() {
-    register_nav_menu('links-menu',__( 'Links Menu' ));
-  }
-  add_action( 'init', 'register_my_links_menu' );
-
-  function register_my_extra_menu() {
-    register_nav_menu('extra-menu',__( 'Extra Menu' ));
-  }
-  add_action( 'init', 'register_my_extra_menu' );
-
+	
+  add_theme_support( 'post-thumbnails' );
