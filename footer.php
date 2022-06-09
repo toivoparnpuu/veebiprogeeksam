fab269fcf7fa259543b5b5df3e1f7056a9d70c4e
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-06-09 15:09:44 +0300

    telefoni sisu

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div>Telefon: +372 555 666 333</div>
+    <div>+372 555 666 333</div>
     <div>tparnpuu@tlu.ee</div>
     <div>Ellandvahe tee 11, Jõelähtme, Harjumaa, Eesti</div>
 </footer>
