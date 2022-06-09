db1e6c9aa701b83e6932c04cd6e71eb0c85d9f7b
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-06-09 15:08:59 +0300

    footeri teksti lisamine

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer>
-    <div>Ettevõtte XYZ, toimetamas</div>
+    <div>Telefon: +372 555 666 333</div>
+    <div>tparnpuu@tlu.ee</div>
+    <div>Ellandvahe tee 11, Jõelähtme, Harjumaa, Eesti</div>
 </footer>
 </body>
 </html>
