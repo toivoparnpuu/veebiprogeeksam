9225d2e6b1d05ecb568f382b88889ad3c29db7bd
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-06-09 15:38:59 +0300

    html-i korrastus

--- index.php
+++ index.php
@@ -2,47 +2,44 @@
 get_header();
 if (have_posts()) {
 ?>
-  <div class="subpagecontainter">
-    <?php
-    while (have_posts()) {
-      the_post(); ?>
-      <section>
-        <h2><?php the_title(); ?></h2>
 
-          <div>
-            <?php if (has_post_thumbnail()) {
-              the_post_thumbnail();
-            }
-            ?>
-          
-          <h2>Nimi:
-            <?php
-            echo (get_post_meta(get_the_ID(), $key = 'fullname', $single = true));
-            ?>
-          </h2>
-          <p>Tutvustus:
-            <?php
-            echo (get_post_meta(get_the_ID(), $key = 'short_bio', $single = true));
-            ?>
-          </p>
-          <p>e-mail:
-            <?php
-            echo (get_post_meta(get_the_ID(), $key = 'email', $single = true));
-            ?>
-          </p>
-          <p>Amet:
-            <?php
-            echo (get_post_meta(get_the_ID(), $key = 'job_role', $single = true));
-            ?>
-          </p>
-          </div>
-          </section>
+  <?php
+  while (have_posts()) {
+    the_post(); ?>
+    <section>
+      <h2><?php the_title(); ?></h2>
 
+      <div>
+        <?php if (has_post_thumbnail()) {
+          the_post_thumbnail();
+        }
+        ?>
+
+        <h2>Nimi:
+          <?php
+          echo (get_post_meta(get_the_ID(), $key = 'fullname', $single = true));
+          ?>
+        </h2>
+        <p>Tutvustus:
+          <?php
+          echo (get_post_meta(get_the_ID(), $key = 'short_bio', $single = true));
+          ?>
+        </p>
+        <p>e-mail:
+          <?php
+          echo (get_post_meta(get_the_ID(), $key = 'email', $single = true));
+          ?>
+        </p>
+        <p>Amet:
+          <?php
+          echo (get_post_meta(get_the_ID(), $key = 'job_role', $single = true));
+          ?>
+        </p>
       </div>
-
-    <?php
-    }
-    ?>
+    </section>
+  <?php
+  }
+  ?>
 
 
 <?php
